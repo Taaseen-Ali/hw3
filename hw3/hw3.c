4ceeb41c57b27fbb09af3a9da3f6a7abbdce18a1
Taaseen Ali <taaseen.ali89@gmail.com>
2018-09-19 18:20:26 -0400

    added hw

---
+++ hw3/hw3.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+  
+  srand(time(NULL));
+  
+  int ar[10];
+  int ar_two[10];
+
+
+  //populating ar
+  for(int i=0; i<9; i++){
+    ar[i] = rand();
+  }
+  ar[9] = 0;
+
+  //printing ar
+  for(int i=0; i<10; i++)
+    printf("ar[%d]: %d\n", i, ar[i]);
+    
+  int *num = &ar;
+  int *num_two = &ar_two;
+  num+=9;
+    //transfering values from ar to ar_two
+  for(int i=0; i<10; i++)
+    *(num_two+i) = *(num-i);
+    
+  printf("\n======new array=======\n");
+
+  //printing ar_two
+  for(int i=0; i<10; i++)
+    printf("ar_two[%d]: %d\n", i, ar_two[i]);
+
+}
